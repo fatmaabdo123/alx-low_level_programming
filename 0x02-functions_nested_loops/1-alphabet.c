672ec9e81a4106f3e85dcfb0177b7f10094eedfd
fatmaabdo123 <fatma79510@gmail.com>
2023-09-13 15:28:55 -0700

    my pro76554097938958

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,17 @@
 
 /**
  * print_alphabet - Entry point
- * Return:0
+ *
+ * Return: Always 0 (Success)
  */
 
 void print_alphabet(void)
 {
-	char c = 'a';
+	int i;
 
-	while (c <= 'z')
+	for (i = 97; i<= 122; i++)
 	{
-		_putchar(c);
-		c++;
-
+		_putchar(i);
 	}
 	_putchar('\n');
-
 }
